18b3f32ca9953e823226324941e0e5efa4f406a9
Timberfang <timberfang.code@pm.me>
2025-04-21 17:59:08 -0500

    refactor: Make input more flexible

--- src/Services/InputService.cs
+++ src/Services/InputService.cs
@@ -6,30 +6,31 @@ namespace GameBox.Services;
 
 public static class InputService
 {
+    // TODO: Update dictionary as types added
+    private static readonly Dictionary<Type, MediaType> TypeToEnum = new()
+    {
+        { typeof(Game), MediaType.Game }
+    };
+
     public static Game NewGame()
     {
-        AnsiConsole.WriteLine("Adding a new game...");
-        AnsiConsole.WriteLine();
-        Game output = new(
-            AnsiConsole.Ask<string>("Name:"),
-            AnsiConsole.Ask<string>("Description:"),
-            GetGamePlatform(),
-            AnsiConsole.Ask<int>("Release Year:"),
-            AnsiConsole.Ask<string>("Creator:")
-        );
+        Game output = NewMedia<Game>();
+        output.Platform = GetGamePlatform();
         return output;
     }
 
-    public static Game ChooseGame()
+    public static T ChooseMedia<T>() where T : IMedia
     {
-        string ChosenGame = AnsiConsole.Prompt(
+        MediaType type = TypeToEnum[typeof(T)];
+
+        string ChosenMedia = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
-                .Title("Choose a [blue]game[/]:")
-                .AddChoices(FileService.GetMedia(MediaType.Game))
+                .Title($"Choose a [blue]{nameof(T)}[/]:")
+                .AddChoices(FileService.GetMedia(type))
                 .UseConverter(Path.GetFileNameWithoutExtension)
         );
 
-        return FileService.LoadMedia<Game>(ChosenGame, MediaType.Game);
+        return FileService.LoadMedia<T>(ChosenMedia, type);
     }
 
     private static GamePlatform GetGamePlatform()
@@ -40,5 +41,21 @@ public static class InputService
                 .AddChoices(Enum.GetValues<GamePlatform>())
                 .UseConverter(x => x.Humanize())
         );
+    }
+
+    private static T NewMedia<T>() where T : IMedia, new()
+    {
+        AnsiConsole.WriteLine($"Creating a new {nameof(T)}...");
+        AnsiConsole.WriteLine();
+
+        T output = new()
+        {
+            Name = AnsiConsole.Ask<string>("Name:"),
+            Description = AnsiConsole.Ask<string>("Description:"),
+            Year = AnsiConsole.Ask<int>("Release Year:"),
+            Creator = AnsiConsole.Ask<string>("Creator:")
+        };
+
+        return output;
     }
 }
