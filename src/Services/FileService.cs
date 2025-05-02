a1b017eb6940d103e50093324e5922a7ac222819
Timberfang <timberfang.code@pm.me>
2025-05-02 11:44:42 -0500

    feat: Add basic CLI

--- src/Services/FileService.cs
+++ src/Services/FileService.cs
@@ -5,77 +5,78 @@ namespace GameBox.Services;
 
 public static class FileService
 {
-    private static readonly string ConfigPath = Path.Join(Environment.CurrentDirectory, "config");
-    private static readonly JsonSerializerOptions JsonOptions = new()
-    {
-        IgnoreReadOnlyProperties = true,
-        WriteIndented = true
-    };
-    private static readonly Dictionary<Type, string> LibraryName = new()
-    {
-        { typeof(Game), "games" },
-        // { typeof(Media), "movies" },
-        // { typeof(Show), "shows" }
-    };
+	private static readonly string ConfigPath = Path.Join(Environment.CurrentDirectory, "config");
 
-    public static IEnumerable<string> GetMedia<T>() where T : IMedia
-    {
-        Initialize<T>();
-        return Directory.GetFiles(GetLibraryPath<T>(), "*.json");
-    }
+	private static readonly JsonSerializerOptions JsonOptions = new()
+	{
+		IgnoreReadOnlyProperties = true,
+		WriteIndented = true
+	};
 
-    public static void SaveMedia<T>(T media) where T : IMedia
-    {
-        Initialize<T>();
-        string TargetPath = Path.Combine(GetLibraryPath<T>(), GetSafePath(media.Name) + ".json");
-        Serialize(media, TargetPath);
-    }
+	private static readonly Dictionary<Type, string> LibraryName = new()
+	{
+		{ typeof(Game), "games" }
+		// { typeof(Media), "movies" },
+		// { typeof(Show), "shows" }
+	};
 
-    public static T LoadMedia<T>(string name) where T : IMedia
-    {
-        Initialize<T>();
-        string TargetPath = Path.Combine(GetLibraryPath<T>(), name);
-        if (!TargetPath.EndsWith(".json")) { TargetPath += ".json"; }
-        return Deserialize<T>(TargetPath);
-    }
+	public static IEnumerable<string> GetMedia<T>() where T : IMedia
+	{
+		Initialize<T>();
+		return Directory.GetFiles(GetLibraryPath<T>(), "*.json");
+	}
 
-    public static IEnumerable<T> LoadAllMedia<T>() where T : IMedia
-    {
-        Initialize<T>();
-        List<T> output = [];
-        foreach (string file in GetMedia<T>()) { output.Add(LoadMedia<T>(file)); }
-        return output;
-    }
+	public static void SaveMedia<T>(T media) where T : IMedia
+	{
+		Initialize<T>();
+		string targetPath = Path.Combine(GetLibraryPath<T>(), GetSafePath(media.Name) + ".json");
+		Serialize(media, targetPath);
+	}
 
-    private static void Initialize<T>() where T : IMedia
-    {
-        string[] Directories = [ConfigPath, GetLibraryPath<T>()];
+	public static T LoadMedia<T>(string name) where T : IMedia
+	{
+		Initialize<T>();
+		string targetPath = Path.Combine(GetLibraryPath<T>(), name);
+		if (!targetPath.EndsWith(".json")) targetPath += ".json";
+		return Deserialize<T>(targetPath);
+	}
 
-        foreach (string DirectoryPath in Directories) { Directory.CreateDirectory(DirectoryPath); }
-    }
+	public static IEnumerable<T> LoadAllMedia<T>() where T : IMedia
+	{
+		Initialize<T>();
+		List<T> output = [];
+		output.AddRange(GetMedia<T>().Select(LoadMedia<T>));
+		return output;
+	}
 
-    private static void Serialize<T>(T obj, string target)
-    {
-        if (File.Exists(target)) { throw new InvalidOperationException(""); }
-        File.WriteAllText(target, JsonSerializer.Serialize(obj, JsonOptions));
-    }
+	private static void Initialize<T>() where T : IMedia
+	{
+		string[] directories = [ConfigPath, GetLibraryPath<T>()];
 
-    private static T Deserialize<T>(string target)
-    {
-        if (!File.Exists(target)) { throw new FileNotFoundException(""); }
-        return JsonSerializer.Deserialize<T>(File.ReadAllText(target)) ?? throw new Exception();
-    }
+		foreach (string directoryPath in directories) Directory.CreateDirectory(directoryPath);
+	}
 
-    private static string GetLibraryPath<T>() where T : IMedia
-    {
-        return Path.Join(ConfigPath, LibraryName[typeof(T)]);
-    }
+	private static void Serialize<T>(T obj, string target)
+	{
+		if (File.Exists(target)) throw new InvalidOperationException("");
+		File.WriteAllText(target, JsonSerializer.Serialize(obj, JsonOptions));
+	}
 
-    private static string GetSafePath(string path)
-    {
-        // Colons get special handling, see https://wiki.no-intro.org/index.php?title=Naming_Convention#Characters
-        path = path.Replace(": ", " - ");
-        foreach (char c in Path.GetInvalidFileNameChars()) { path = path.Replace(c, '_'); }
-        return path;
-    }
+	private static T Deserialize<T>(string target)
+	{
+		if (!File.Exists(target)) throw new FileNotFoundException($"File at '{target}' not found.");
+		return JsonSerializer.Deserialize<T>(File.ReadAllText(target)) ?? throw new Exception();
+	}
+
+	private static string GetLibraryPath<T>() where T : IMedia
+	{
+		return Path.Join(ConfigPath, LibraryName[typeof(T)]);
+	}
+
+	private static string GetSafePath(string path)
+	{
+		// Colons get special handling, see https://wiki.no-intro.org/index.php?title=Naming_Convention#Characters
+		path = path.Replace(": ", " - ");
+		return Path.GetInvalidFileNameChars().Aggregate(path, (current, c) => current.Replace(c, '_'));
+	}
 }
--- src/Services/InputService.cs
+++ src/Services/InputService.cs
@@ -1,52 +1,68 @@
-using Spectre.Console;
 using GameBox.Media;
+using System.CommandLine;
+using System.Text.RegularExpressions;
 
 namespace GameBox.Services;
 
-public static class InputService
+public static partial class InputService
 {
-    public static Game NewGame()
-    {
-        Game output = NewMedia<Game>();
-        output.Platform = GetGamePlatform();
-        return output;
-    }
+	public static async Task<int> StartCommandLine(string[] args)
+	{
+		Option<string> nameOption = new Option<string>("--name");
+		Option<string> descriptionOption = new Option<string>("--description", getDefaultValue: () => string.Empty);
+		Option<int> yearOption = new Option<int>("--year", getDefaultValue: () => 1970);
+		Option<string> creatorOption = new Option<string>("--creator", getDefaultValue: () => string.Empty);
+		Option<string> typeOption = new Option<string>("--type");
+		nameOption.IsRequired = true;
+		typeOption.IsRequired = true;
+		
+		RootCommand rootCommand = new RootCommand("Media manager.");
+		Command createCommand = new Command("create")
+		{
+			nameOption,
+			descriptionOption,
+			yearOption,
+			creatorOption
+		};
+		Command readCommand = new Command("read")
+		{
+			nameOption,
+			typeOption
+		};
+		rootCommand.AddCommand(createCommand);
+		rootCommand.AddCommand(readCommand);
+		
+		createCommand.SetHandler((name, description, year, creator) =>
+			{
+				Game output = new Game(name, description, GamePlatform.PC, year, creator);
+				FileService.SaveMedia(output);
+			},
+			nameOption, descriptionOption, yearOption, creatorOption);
+		readCommand.SetHandler((name, type) =>
+			{
+				try
+				{
+					IMedia output = type switch
+					{
+						"game" => FileService.LoadMedia<Game>(name),
+						// "movie" => FileService.LoadMedia<Movie>(name),
+						_ => throw new ArgumentOutOfRangeException(nameof(type))
+					};
 
-    public static T ChooseMedia<T>() where T : IMedia
-    {
-        string ChosenMedia = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title($"Choose a {nameof(T)}:")
-                .AddChoices(FileService.GetMedia<T>())
-                .UseConverter(Path.GetFileNameWithoutExtension)
-        );
+					Console.WriteLine(output.ToString());
+				}
+				catch (FileNotFoundException e)
+				{
+					string filePath = FileExceptionRegex().Match(e.Message).Value;
+					string fileName = Path.GetFileNameWithoutExtension(filePath);
+					Console.WriteLine($"No media of type '{type}' named '{fileName}' was found.");
+				}
+			},
+			nameOption, typeOption);
+		
+		return await rootCommand.InvokeAsync(args);
+	}
 
-        return FileService.LoadMedia<T>(ChosenMedia);
-    }
-
-    private static GamePlatform GetGamePlatform()
-    {
-        return AnsiConsole.Prompt(
-            new SelectionPrompt<GamePlatform>()
-                .Title("Choose a platform for this game:")
-                .AddChoices(Enum.GetValues<GamePlatform>())
-                .UseConverter(x => Game.PlatformFriendlyName[x])
-        );
-    }
-
-    private static T NewMedia<T>() where T : IMedia, new()
-    {
-        AnsiConsole.WriteLine($"Creating a new {nameof(T)}...");
-        AnsiConsole.WriteLine();
-
-        T output = new()
-        {
-            Name = AnsiConsole.Ask<string>("Name:"),
-            Description = AnsiConsole.Ask<string>("Description:"),
-            Year = AnsiConsole.Ask<int>("Release Year:"),
-            Creator = AnsiConsole.Ask<string>("Creator:")
-        };
-
-        return output;
-    }
+    [GeneratedRegex(@"(?<=').+(?=')")]
+    private static partial Regex FileExceptionRegex();
 }
